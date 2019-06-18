d8dc49a603a54e3174afe027de70b48a1801cec0
patryczkov <d1metyloam1na>
2019-06-18 14:41:27 +0200

    Add Card repository with methods: addCard() and cardIterator()

--- src/main/java/com/codecool/jakotako/battleofcards/CardRepository.java
+++ src/main/java/com/codecool/jakotako/battleofcards/CardRepository.java
@@ -1,4 +1,42 @@
 package com.codecool.jakotako.battleofcards;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
 public class CardRepository {
+    private Iterator<Card> cardIterator;
+    private List<Card> cards = new ArrayList<>();
+
+    public CardRepository() {
+        cardIterator = new CardIterator();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public Iterator<Card> getIterator() {
+        return cardIterator;
+    }
+
+    private class CardIterator implements Iterator<Card> { //do i have always to Override methods?
+        int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < cards.size();
+        }
+
+        @Override
+        public Card next() {
+            return hasNext() ? cards.get(index++) : null;
+        }
+//        @Override
+//        public void remove(){
+//            return;
+//        }
+
+    }
 }
