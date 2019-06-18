2fdaa1a5ceda429a970cb0498a7a4fc9afc97914
patryczkov <d1metyloam1na>
2019-06-18 14:18:17 +0200

    Add Card class with methods

--- src/main/java/com/codecool/jakotako/battleofcards/Card.java
+++ src/main/java/com/codecool/jakotako/battleofcards/Card.java
@@ -1,14 +1,29 @@
+package com.codecool.jakotako.battleofcards;
 
-public class Card{
+import java.util.HashMap;
+import java.util.Map;
+
+public class Card {
+    private String id;
+    private String description;
+    private Map<String, Integer> fields = new HashMap();
+
+
 
     public Card(String id, String description){
+        this.id = id;
+        this.description = description;
+    }
+    public Map<String,Integer> getFields(){
+        return this.fields;
+    }
 
+    public void setCardValueById(String id, int value){ //use primitives always except you need to use boxes
+        this.fields.put(id, value);
     }
     public String getDescription(){
+            return this.description;
+        }
+
 
     }
-    public int getValueById(){
-
-    }
-
-}
